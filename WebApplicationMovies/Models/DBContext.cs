f8adc5ef270c67de224c8ea6f920a5e739caf49b
jackkaikay <53180658+jackkaikay@users.noreply.github.com>
2020-02-12 15:18:56 +0000

    First Commit
    Unworking Currently after controll added

---
+++ WebApplicationMovies/Models/DBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationMovies.Models
+{
+    public class DBContext : DbContext
+    {
+
+        public DbSet<Film> Films { get; set; }
+    }
+}
---
+++ WebApplicationMovies/Models/Film.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationMovies.Models
+{   [Table("FILM")]
+    public class Film
+    {
+        [Column("film_id")]
+        public int FilmID { get; set; }
+        [Column("film_title")]
+        public string FilmTitle { get; set; }
+        [Column("film_genre")]
+        public string FilmGenre { get; set; }
+        [Column("film_desc")]
+        public string FilmDesc { get; set; }
+        [Column("film_date")]
+        public DateTime FilmReleaseDate { get; set; }
+        [Column("film_img")]
+        public string FilmImage { get; set; }
+
+
+        public string FilmSecTrimmed;
+
+    }
+}
