da7f3f2b7dfa6b31cea0cf2348cbe3df3e0ade03
jackkaikay <53180658+jackkaikay@users.noreply.github.com>
2020-02-18 16:23:52 +0000

    Pre Controller.cs edit

--- WebApplicationMovies/Models/Film.cs
+++ WebApplicationMovies/Models/Film.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -9,20 +10,52 @@ namespace WebApplicationMovies.Models
     public class Film
     {
         [Column("film_id")]
-        public int FilmID { get; set; }
+        [Display(Name = "ID")]
+        public int FilmID { get; set; }      
+
+        [Required]
         [Column("film_title")]
+        [Display(Name = "Title")]
         public string FilmTitle { get; set; }
+
+        [Required]
         [Column("film_genre")]
+        [Display(Name = "Genre")]
         public string FilmGenre { get; set; }
+
+        [Required]
         [Column("film_desc")]
+        [Display(Name = "Description")]
+        [DataType(DataType.MultilineText)]
         public string FilmDesc { get; set; }
-        [Column("film_date")]
-        public DateTime FilmReleaseDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}",
+            ApplyFormatInEditMode = true)]
+        [Column("film_r_date")]
+        [Display(Name = "Release Date")]
+        public DateTime? FilmReleaseDate{ get; set; }
+
         [Column("film_img")]
+        [Display(Name = "Image")]
         public string FilmImage { get; set; }
 
 
         public string FilmSecTrimmed;
 
+
+        public string FilmDescTrimmed
+        {
+
+            get
+            {
+
+                if ((FilmDesc.Length) > 100)
+
+                    return FilmDesc.Substring(0, 100) + "...";
+                else
+                    return FilmDesc;
+            }
+        }
     }
 }
